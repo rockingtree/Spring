b809cc796b4200320c701652dcb8a031fb97b944
rockingtree <chansr@live.cn>
2019-01-24 17:09:57 +0800

    filling content

--- PrinterTest/src/main/java/com/u3/entity/ink/GreyInk.java
+++ PrinterTest/src/main/java/com/u3/entity/ink/GreyInk.java
@@ -9,7 +9,7 @@ public class GreyInk implements Ink {
     @Override
     public String getColor(int r, int g, int b) {
         //rgb值相等就是灰色
-        int c = (r+g+b)/3;
+        int c = (r + g + b) / 3;
         Color color = new Color(c, c, c);
         return "#" + Integer.toHexString(color.getRGB()).substring(2);
     }
